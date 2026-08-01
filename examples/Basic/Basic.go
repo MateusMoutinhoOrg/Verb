2cf9ec857a08505294fbe9b7b263742ba9bee068
mateusmoutinho <mateusmoutinho01@gmail.com>
2026-08-01 18:54:33 -0300

    feat: pass os.Args to verb adapter in Basic example

--- examples/Basic/Basic.go
+++ examples/Basic/Basic.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"os"
+
 	verbadapter "github.com/MateusMoutinhoOrg/Verb/adapters/standard"
 	verblib "github.com/MateusMoutinhoOrg/Verb/sandbox"
 )
 
 func main() {
 	// Build deps via an adapter (JSON file store + real clock) and inject.
-	lib := verblib.New(verbadapter.New())
+	lib := verblib.New(verbadapter.New(os.Args))
 
 }
