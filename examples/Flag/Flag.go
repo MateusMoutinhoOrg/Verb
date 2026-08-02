c54dee0be515c9d1cbc5637cd858a3018b357c64
mateusmoutinho <mateusmoutinho01@gmail.com>
2026-08-01 21:39:38 -0300

    refactor: remove manual Action configuration from Flag example

--- examples/Flag/Flag.go
+++ examples/Flag/Flag.go
@@ -11,27 +11,6 @@ func main() {
 	// Build deps via an adapter (JSON file store + real clock) and inject.
 	lib := verblib.New(verbadapter.New(os.Args))
 
-	lib.setActions([]verblib.Action{
-		verblib.Action{
-			Triggers: []verblib.Trigger{
-
-				verblib.ArgTrigger{
-
-					ExpectedValue: "commit",
-					//will be triggered if has commit word in args
-					// from  1 to 1 means that both argv[0] and argv[1] can be commit
-					PlotageArea: []uint{1, 1},
-				},
-			},
-			Entries: []verblib.Entry{
-				verblib.FlagEntry{
-					Name:        "message",
-					Identifiers: []string{"-m", "--m", "-message", "--message"},
-					Required:    true,
-				},
-			},
-		},
-	})
 }
 
 // test by
